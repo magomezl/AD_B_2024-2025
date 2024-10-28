900124d40ac4142874117c134122c521b14db000
Administrador <Administrador@W10>
2024-10-28 13:58:30 +0100

    Ejercicio 13

--- Ficheros.iesjulianmarias.es/src/main/java/excel/Ejercicio13.java
+++ Ficheros.iesjulianmarias.es/src/main/java/excel/Ejercicio13.java
@@ -1,7 +1,9 @@
 package excel;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 import java.io.File;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -19,7 +21,8 @@ public class Ejercicio13 {
 	public static void main(String[] args) {
 		try {
 			wb = new XSSFWorkbook(new FileInputStream(new File(Utilidades.getRutadatos()+ DOCTRABAJO_IN)));
-			buscarEstacionesPorMarca("Wenea");
+			//buscarEstacionesPorMarca("Wenea");
+			buscarEstacionesPorCiudad("salamanca");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -48,4 +51,43 @@ public class Ejercicio13 {
 	
 	// Para casa crear un método que muestre los puntos de carga de una determinada ciudad 
 	// que se pasará como parámetro
+	// Añadimos una hoja al documento excel con los puntos de cada ciudad que busquemos
+	
+	private static void buscarEstacionesPorCiudad(String ciudad) {
+		Scanner sc = new Scanner(System.in);
+		try {
+		
+		Sheet hoja = wb.getSheetAt(0);
+		Sheet newHoja = wb.createSheet(ciudad);
+		int numFila = 1, newNumFila=0;
+		Row fila = hoja.getRow(numFila);
+		
+		// Muestro las localizaciones de los puntos de carga del operador pasado como parámetro
+		System.out.println("PUNTOS DE RECARGA EN " + ciudad );
+		while(fila!=null) {
+			Cell celdaBusqueda = fila.getCell(1);
+			if (celdaBusqueda!=null) {
+				if (celdaBusqueda.getStringCellValue().toLowerCase().contains("("+ciudad+")")) {
+					System.out.println("----->" + fila.getCell(0).getStringCellValue() + "(" + 
+							fila.getCell(1).getStringCellValue());
+					Row newFila = newHoja.createRow(newNumFila++);
+					newFila.createCell(0).setCellValue(fila.getCell(0).getStringCellValue());
+					newFila.createCell(1).setCellValue(fila.getCell(1).getStringCellValue());
+				}
+			}
+			fila = hoja.getRow(numFila++);
+		}
+		try {
+			wb.write(new FileOutputStream(Utilidades.getRutadatos()+ DOCTRABAJO_IN));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		}catch (IllegalArgumentException e) {
+			System.out.println("Ya se han encontrado los datos de " + ciudad + "¿Desea sobreescribir? (S/N): ");
+			if (sc.next().equals("S")){
+				wb.removeSheetAt(wb.getSheetIndex(wb.getSheet(ciudad)));
+				buscarEstacionesPorCiudad(ciudad);
+			}
+		}
+	}
 }
