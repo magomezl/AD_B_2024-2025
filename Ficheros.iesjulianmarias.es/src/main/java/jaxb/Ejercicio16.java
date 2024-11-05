b6e7129495a003253f7110b6cf44c94325e6847e
Administrador <Administrador@W10>
2024-11-05 17:42:07 +0100

    Ejercicio 17 para casa ampliación

--- Ficheros.iesjulianmarias.es/src/main/java/jaxb/Ejercicio16.java
+++ Ficheros.iesjulianmarias.es/src/main/java/jaxb/Ejercicio16.java
@@ -1,8 +1,10 @@
 package jaxb;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import ficheros.Utilidades;
@@ -16,13 +18,14 @@ import jaxb.clasesEjercicio16.Examen;
 public class Ejercicio16 {
 	private static JAXBContext jC;
 	private static Examen examen;
+	private static ArrayList<Autor> autores;
+	
 	private static final String  DOCUMENTO_XML = "ejercicio16.xml";
 	
 	
 	public static void main(String[] args) {
 		cargarEnmemoriaXML();
-//		menu();
-		leerAutores();
+		menu();
 		persistirEnFicheroXML();
 	}
 	
@@ -52,76 +55,147 @@ public class Ejercicio16 {
 			jC = JAXBContext.newInstance(Examen.class);
 			Unmarshaller uM = jC.createUnmarshaller();
 			examen = (Examen) uM.unmarshal(new File(Utilidades.getRutadatos()+ DOCUMENTO_XML));
+			autores = examen.getListaAutores();
 			
 		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
 	}
 
-//	public static void menu() {
-//
-//		Scanner sn = new Scanner(System.in);
-//		boolean salir = false;
-//		
-//
-//		while(!salir) {
-//			System.out.println("\n1. Leer autores");
-//			System.out.println("2. Eliminar autor");
-//			System.out.println("3. Añadir autor");
-//			System.out.println("4. Modificar autor. Entidad de trabajo y/o puesto");
-//			System.out.println("5. Salir");
-//			try {
-//				System.out.println("Escribe una de las opciones: ");
-//				switch(sn.nextInt()){
-//				case 1:
-//					System.out.println("LEYENDO AUTORES");
-//					leer();
-//					break;
-//				case 2:
-//					System.out.println("ELIMINAR AUTOR");
-//					
-//					System.out.println("indique la posición a consultar (valor numérico): ");
-//					posicion = sn.nextInt();
-//					int value;
-//					leer(posicion);
-//					break;
-//				case 3:
-//					System.out.println("AÑADIENDO AUTOR");
-//					System.out.println("indique el valor del mes (tres letras): ");
-//					mes = sn.next();
-//					System.out.println("indique temperatura mínima: ");
-//					minima = sn.nextInt();
-//					System.out.println("indique temperatura máxima: ");
-//					maxima = sn.nextInt();
-//					System.out.println(escribir(mes, minima, maxima)?"Temperaturas añadidas correctamente":"ERROR: Temperaturas no añadidas");
-//					break;
-//				case 4:
-//					System.out.println("MODIFICANDO VALOR EN POSICIÓN");
-//					System.out.println("indique posición: ");
-//					posicion = sn.nextInt();
-//					System.out.println("indique el valor del mes (tres letras): ");
-//					mes = sn.next();
-//					System.out.println("indique temperatura mínima: ");
-//					minima = sn.nextInt();
-//					System.out.println("indique temperatura máxima: ");
-//					maxima = sn.nextInt();
-//					System.out.println(escribir(mes, posicion, minima, maxima)?"Valor modificado correctamente":"ERROR: Valor no modificado");
-//					break;
-//				case 5:
-//					salir = true;
-//					break;
-//				default:
-//					System.out.println("Solo números entre 1 y 5");
-//					break;
-//				}
-//			}catch(InputMismatchException e) {
-//				System.out.println("Debes escribir un número");
-//				sn.next();
-//			}
-//		}
-//		
-//	}
+	public static void menu() {
+		Scanner sc = new Scanner(System.in);	
+		boolean salir = false;
+		while(!salir) {
+			System.out.println("\n1. Leer autores");
+			System.out.println("2. Eliminar autor");
+			System.out.println("3. Añadir autor");
+			System.out.println("4. Modificar autor. Entidad de trabajo y/o puesto");
+			System.out.println("5. Salir");
+			try {
+				System.out.println("Escribe una de las opciones: ");
+				switch(sc.nextInt()){
+				case 1:
+					System.out.println("LEYENDO AUTORES");
+					leerAutores();
+					
+					break;
+				case 2:
+					System.out.println("ELIMINAR AUTOR");
+					System.out.println("Indique el id del autor a eliminar: ");
+					System.out.println(borrarAutor(sc.next())?"El autor se eliminó con éxito":"El autor no existe");
+					break;
+				case 3:
+					System.out.println("AÑADIENDO AUTOR");
+					
+					System.out.println(anadirAutor(pedirDatosAutor())?"Autor añadido con éxito":"Autor NO añadido");
+					break;
+				case 4:
+					System.out.println("MODIFICANDO DATOS DE AUTOR");
+					System.out.println("Indique el id del autor a modificar: ");
+					String idAutor = sc.nextLine();
+					System.out.println("Nueva Entidad de Trabajo: ");
+					String newET = sc.nextLine();
+					System.out.println("Nuevo Puesto: ");
+					String newP = sc.nextLine();
+					System.out.println(modificarAutor(idAutor, newET, newP)?"Autor modificado con éxito":"Autor NO modificado");
+					break;
+				case 5:
+					salir = true;
+					break;
+				default:
+					System.out.println("Solo números entre 1 y 5");
+					break;
+				}
+			}catch(InputMismatchException e) {
+				System.out.println("Debes escribir un número");
+				sc.next();
+			}catch (NoSuchElementException e) {
+				System.out.println("ERROR");
+				
+			}
+		}
+		
+	}
+	
+	private static boolean modificarAutor(String idAutor, String newET, String newP) {
+		
+		Autor autorModificar = localizarAutor(idAutor);
+		autorModificar.setEntidadTrabajo(newET);
+		autorModificar.setPuesto(newP);
+		if (autores.set(localizarLugarAutor(idAutor), autorModificar)!=null)
+			return true;
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param idAutor
+	 * @return posición del autor cuyo id se pasa como parámetro o -1 en caso se no existir
+	 */
+	private static int localizarLugarAutor(String idAutor) {
+		Iterator<Autor> it = autores.iterator();
+		Autor autorEncurso;
+		int i = 0;
+		while(it.hasNext()) {
+			autorEncurso = it.next();
+			if (autorEncurso.getId().equals(idAutor)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @return true si se elimina el elemento y false si no se elimnina
+	 */
+	private static boolean borrarAutor(String id) {
+		return autores.remove(localizarAutor(id));
+		
+	}
+
+	private static Autor localizarAutor(String id) {
+		Iterator<Autor> it = autores.iterator();
+		Autor autorEncurso;
+		while(it.hasNext()) {
+			autorEncurso = it.next();
+			if (autorEncurso.getId().equals(id)) {
+				return autorEncurso;
+			}
+		}
+		return null;
+	}
+
+	private static boolean anadirAutor(Autor autor) {
+		return autores.add(autor);
+	}
+
+	private static Autor pedirDatosAutor() {
+		Scanner sn = new Scanner(System.in);
+		Autor autor = new Autor();
+		
+		System.out.println("Identificador: ");
+		autor.setId(sn.nextLine());
+		
+		System.out.println("Nombre: ");
+		autor.setNombre(sn.nextLine());
+		System.out.println("Primer Apellido: ");
+		autor.setApellido1(sn.nextLine());
+		System.out.println("Segundo Apellido: ");
+		autor.setApellido2(sn.nextLine());
+		System.out.println("Entidad de Trabajo: ");
+		String eT = sn.nextLine();
+		autor.setEntidadTrabajo(eT);
+		System.out.println("Puesto: ");
+		String p = sn.nextLine();
+		sn.close();
+		autor.setPuesto(p);
+		
+		return autor;
+	}
 	
 }
